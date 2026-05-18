6d053110f4a2cae92785f4ec13e40da739bd219f
Your Name <ahosanulhaquenoor@gmail.com>
2026-05-18 22:12:54 +0600

    app

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const uri = "mongodb+srv://novamed:PY22qgU03VH6L81d@cluster0.agkguij.mongodb.net/?appName=Cluster0";
+const uri = process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
